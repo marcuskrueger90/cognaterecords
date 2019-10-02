7c475b7fa197d57314c912b1f50ea1f4427a64fc
richard mcgirt <richardmcgirt@richards-MBP.attlocal.net>
2019-10-02 14:17:46 -0400

    new login html and js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+(function(){
+
+  // Your web app's Firebase configuration
+  var Config = {
+    apiKey: "AIzaSyCPXJpUdfqPC8FaOA0ITNkdX-z9TJDrC9M",
+    authDomain: "musicapi-9029a.firebaseapp.com",
+    databaseURL: "https://musicapi-9029a.firebaseio.com",
+    projectId: "musicapi-9029a",
+    storageBucket: "",
+    messagingSenderId: "948545909374",
+    appId: "1:948545909374:web:7ab9065c437d3059a41e2f",
+    measurementId: "G-SYXWD3108H"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(Config);
+
+})()
